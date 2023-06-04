4c69e9d089a5adea0f45341371d87fea16a35f42
sokrat <sokrat1798@sokrat1798.0.2.15>
2023-06-04 23:49:19 +0700

    Updet file

--- writting.cpp
+++ writting.cpp
@@ -1,26 +1,26 @@
-#include <fstream>
+#include <fstream> 
+#include <string>  
+#include <iostream> 
+#include <Windows.h> 
 #include <string> 
-#include <iostream>
-#include <Windows.h>
-#include <string>
-//#include "SD.h"
-using namespace std;
-
-int wrote()
-{
-    std::string line;
-    std::ofstream out;
-    std::ifstream in("code.cpp");
-    out.open("file.txt");
-    if (in.is_open())
-    {
-        while (std::getline(in, line))
-        {
-            out << line << std::endl;
-        }
-    }
-    out.close();
-    std::cout << "File C++ is writting in file.txt" << std::endl;
-    in.close();
-    return 0;
+ 
+using namespace std; 
+ 
+int wrote(string str) 
+{ 
+    std::string line; 
+    std::ofstream out; 
+    std::ifstream in(str); 
+    out.open("file.txt"); 
+    if (in.is_open()) 
+    { 
+        while (std::getline(in, line)) 
+        { 
+            out << line << std::endl; 
+        } 
+    } 
+    out.close(); 
+    std::cout << "File C++ is writting in file.txt" << std::endl; 
+    in.close(); 
+    return 0; 
 }
