52a37c4467246befc97d3bfb5487e3ae006bbf49
sokrat <sokrat1798@sokrat1798.0.2.15>
2023-06-09 17:50:38 +0700

    :)

--- main.cpp
+++ main.cpp
@@ -11,10 +11,15 @@ int cheking();
 int wrote(string str); 
 string window(); 
 int main() { 
-    cout << "Privet hozain, now i work" << endl; 
-    string sf = window(); 
-    if (sf != "0") { 
-        wrote(sf); 
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "privet hozain, now i work" << endl;
+    string sf = window();
+    if (sf == "-1") {
+        return 0;
+    }
+    else if (sf != "0") {
+        wrote(sf);
     } 
     int countmemory = cheking(); 
     searching(countmemory); 
